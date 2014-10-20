1676a5629d753c153fa9dc054d7a888fa0f3f387
Kelemen Kata <kelemenkata@gmail.com>
2014-10-20 23:08:15 +0200

    highest card

--- poker.py
+++ poker.py
@@ -26,11 +26,36 @@ def is_valid_hand(hand):
             return False
     return True
 
-def is_bigger(bigger, smaller):
+def is_bigger_card(bigger, smaller):
     rank1 , suit1 = split_card(bigger)
     rank2 , suit2 = split_card(smaller)
     rank1index = ALLOWEDRANKS.index(rank1)
     if rank2 in ALLOWEDRANKS[:rank1index]:
         return True
     return False
+
+def is_bigger_hand(biggerhand, smallerhand):
+    biggerhand_highest = get_highest_card(biggerhand)
+    smallerhand_highest = get_highest_card(smallerhand)
+    return is_bigger_card(biggerhand_highest, smallerhand_highest)
+
+def get_highest_card(hand):
+    highest_card = hand[0]
+    for card in hand:
+        if is_bigger_card(card, highest_card):
+            highest_card = card
+    return highest_card
+
+def get_pair(hand):
+    ranklist = []
+    for card in hand:
+        rank, suit = card
+        ranklist.append(rank)
+
+def partition(hand):
+    return []
+
+
+
+
 
--- unit.py
+++ unit.py
@@ -20,11 +20,31 @@ class TestPoker(unittest.TestCase):
         self.assertFalse( poker.is_valid_hand(["AS","7S","AS","10C","JD"]))
 
     def test_bigger_card(self):
-        self.assertTrue( poker.is_bigger("KC", "QD"))
-        self.assertFalse( poker.is_bigger("QD", "KC"))
+        self.assertTrue( poker.is_bigger_card("KC", "QD"))
+        self.assertFalse( poker.is_bigger_card("QD", "KC"))
 
-    def test_is_bigger_hand(self):
-        self.assertTrue(poker.is_bigger_hand)
+    def test_highcard(self):
+        self.assertFalse(poker.is_bigger_hand(
+            ["2S","3S","4S","6C","7D"],
+            ["2S","3S","4S","6C","KD"]))
+        self.assertTrue(poker.is_bigger_hand(
+            ["2S","3S","4S","6C","KD"],
+            ["2S","3S","4S","6C","7D"]))
+
+    def test_one_pair(self):
+        self.assertTrue(poker.get_pair(["2S","3S","3D","6C","7D"]))
+        self.assertTrue(poker.is_bigger_hand(
+            ["2S","3S","3D","6C","7D"],
+            ["2S","3S","4S","6C","KD"]))
+
+    def test_highest_card(self):
+        self.assertTrue(poker.get_highest_card(["AS","7M","7H","10C","JD"])=="AS")
+        self.assertTrue(poker.get_highest_card(["2S","7M","7H","10C","JD"])=="JD")
+
+    def test_partition(self):
+        self.assertEqual(poker.get_highest_card(["AS","AD","AC","10C","10D"]),
+            [["AS","AD","AC"],["10C","10D"]])
+
 ####################################
 
 if __name__ == '__main__':
